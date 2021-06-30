83487c7ccbc9a590fac2739773222c7e7e81cb0b
ElayyanAreej <areej.hossein@gmail.com>
2021-06-30 13:18:14 +0300

    adding css & text

--- js/app2.js
+++ js/app2.js
@@ -1,10 +1,10 @@
 'use strict';
 //index 
 //open hr
-let container2 = document.getElementById('hr');
-let pEl = document.createElement('p');
-pEl.textContent=`Our Shopes Open At 6 am And Closes At 8 pm .`;
-container2.appendChild(pEl);
+// let container2 = document.getElementById('hr');
+// let pEl = document.createElement('p');
+// pEl.textContent=`Our Shopes Open At 6 am And Closes At 8 pm .`;
+// container2.appendChild(pEl);
 
 //locations
 
